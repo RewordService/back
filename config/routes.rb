c7a31c823e2ac496a72e2597936fdaac90c60db5
izszzz <izszzz@.yahoo.co.jp>
2021-02-14 21:14:07 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  scope :api do
     mount_devise_token_auth_for 'User', at: 'auth', controllers: {
       registrations: 'auth/registrations',
     }
@@ -15,6 +16,7 @@ Rails.application.routes.draw do
     resources :contacts, only: :create
 
     get 'reword/info', to: "rewords#info"
+  end
   get '/sitemap', to: redirect("https://reword.s3-ap-northeast-1.amazonaws.com/sitemaps/sitemap.xml.gz")
   get '*path', to: "application#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?

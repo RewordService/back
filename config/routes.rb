c77192015feae58179d2fd2bcdec7a2b36c706ab
izszzz <izszzz@.yahoo.co.jp>
2021-02-14 19:42:53 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-    registrations: 'auth/registrations',
-  }
-  put 'user/reword', to: "rewords#update"
-  patch 'user/reword', to: "rewords#update"
-  get 'user/reword', to: "rewords#show"
+  scope :api do
+    mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+      registrations: 'auth/registrations',
+    }
+    put 'user/reword', to: "rewords#update"
+    patch 'user/reword', to: "rewords#update"
+    get 'user/reword', to: "rewords#show"
 
-  resources :users, only: %i(index show) do
-    collection do
-      get 'latest'
-      get 'ranked'
+    resources :users, only: %i(index show) do
+      collection do
+        get 'latest'
+        get 'ranked'
+      end
     end
-  end
-  resources :contacts, only: :create
+    resources :contacts, only: :create
 
+  end
   get 'reword/info', to: "rewords#info"
     get '*path', to: "application#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
